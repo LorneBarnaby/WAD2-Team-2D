8b09402f21bdfc3efde30b8a7a3a91c5f342137d
Lorne Barnaby <lorne123barnaby@gmail.com>
2022-03-01 18:20:43 +0000

    Added UserProfile model + 2 dummy classes for achievement and prize. Also added CR8 to installed apps.

--- CR8/models.py
+++ CR8/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Prize(models.Model):
+    pass
+
+class Achievement(models.Model):
+    pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profileImage = models.ImageField(upload_to='profile_images', blank=True)
+    currency = models.IntegerField(default=100)
+
+    prizes = models.ManyToManyField(Prize)
+    achievements = models.ManyToManyField(Achievement)
+
+
+
+
--- cr8_project/settings.py
+++ cr8_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CR8'
 ]
 
 MIDDLEWARE = [
