7ac2cd9619a131357ff0154f3ef420d5cd0cd3e3
Jack Kilpatrick <2549732K@student.gla.ac.uk>
2022-03-07 22:41:02 +0000

    Completed leaderboard and profile view in views.py and added temporary html to leaderboard.html and profile.html to test the views

--- CR8/views.py
+++ CR8/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 
-from CR8.models import UserProfile
+from CR8.models import UserProfile, Prize, Achievement
 from .forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
@@ -56,7 +56,14 @@ def about_us(request):
     return render(request, 'cr8/about-us.html')
 
 def leaderboard(request):
-    return render(request, 'cr8/leaderboard.html')
+
+    user_profile_list = UserProfile.objects.order_by('-currency')
+
+    context_dict = {}
+
+    context_dict["user_profile_list"] = user_profile_list
+
+    return render(request, 'cr8/leaderboard.html', context=context_dict)
 
 
 def contact_us(request):
@@ -66,7 +73,29 @@ def faqs(request):
     return render(request, 'cr8/faqs.html')
 
 def profile(request,username):
-    return render(request, 'cr8/profile.html')
+
+    context_dict = {}
+
+    if request.user.is_authenticated and request.user.username == username:
+        context_dict['is_current_user'] = True
+    else:
+        context_dict['is_current_user'] = False
+
+    try:
+        user_profile = UserProfile.objects.get(user__username=username)
+        user_prize_list = Prize.objects.filter(userprofile=user_profile)
+        user_achievement_list = Achievement.objects.filter(userprofile=user_profile)
+
+        context_dict['user_profile'] = user_profile
+        context_dict['user_prize_list'] = user_prize_list
+        context_dict['user_achievement_list'] = user_achievement_list
+
+    except UserProfile.DoesNotExist:
+        context_dict['user_profile'] = None
+        context_dict['user_prize_list'] = None
+        context_dict['user_achievement_list'] = None
+
+    return render(request, 'cr8/profile.html', context=context_dict)
 
 def user_login(request):
     if request.method == 'POST':
