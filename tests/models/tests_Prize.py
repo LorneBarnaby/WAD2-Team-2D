88ba4d1e19dc6460b5dd0a6ab392eb278434426a
Lorne Barnaby <lorne123barnaby@gmail.com>
2022-03-09 12:05:37 +0000

    UserProfile Tests + Prize tests

---
+++ tests/models/tests_Prize.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from CR8.models import UserProfile, User, Prize
+from CR8.rarity import RARITY
+
+
+class UserProfileTest(TestCase):
+    def setUp(self):
+        self.prize = Prize(prizeRarity=RARITY.RARE, prizeName="Test Case Prize")
+        self.prize.save()
+
+    def test_create_prize(self):
+        self.assertEqual(1, Prize.objects.count())
+
+    def test_prize_name(self):
+        self.assertEqual("Test Case Prize", Prize.objects.get(id=self.prize.id).prizeName)
+
+    def test_set_prize_name(self):
+        self.prize.prizeName = "Changed"
+        self.prize.save()
+        self.assertEqual("Changed", Prize.objects.get(id=self.prize.id).prizeName)
+
+    def test_set_prize_rarity(self):
+        self.prize.prizeRarity = str(RARITY.COMMON)
+        self.prize.save()
+        self.assertEqual(str(RARITY.COMMON), Prize.objects.get(id=self.prize.id).prizeRarity)
---
+++ tests/models/tests_UserProfile.py
@@ -0,0 +1,36 @@
+from django.template.defaultfilters import slugify
+from django.test import TestCase
+from CR8.models import UserProfile, User, Prize
+from CR8.rarity import RARITY
+
+
+class UserProfileTest(TestCase):
+    def setUp(self):
+        self.testCaseUser = User(username="testCaseUser")
+        self.testCaseUser.save()
+        self.profile = UserProfile(user=self.testCaseUser, currency=100)
+        self.profile.save()
+
+    def test_create_UserProfile(self):
+        self.assertEqual(1, UserProfile.objects.count())
+
+    def test_create_UserProfile_username(self):
+        self.assertEqual(self.testCaseUser.username, self.profile.user.username)
+
+    def test_create_UserProfile_slug(self):
+        self.assertEqual(slugify(self.testCaseUser.username), self.profile.username_slug)
+
+    def test_add_prizes(self):
+        prizes = [
+            Prize(prizeName="1", prizeRarity=str(RARITY.COMMON), prizeValue=100),
+            Prize(prizeName="2", prizeRarity=str(RARITY.COMMON), prizeValue=101),
+        ]
+        for p in prizes:
+            p.save()
+            self.profile.prizes.add(p)
+        self.assertEqual(2, self.profile.prizes.count())
+
+    def test_give_currenct(self):
+        self.profile.currency += 100
+        self.profile.save()
+        self.assertEqual(200,UserProfile.objects.get(id=self.profile.id).currency)
