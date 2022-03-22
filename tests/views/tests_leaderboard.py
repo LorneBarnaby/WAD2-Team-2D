a7d77e553de29905b84f8151a93122ffeacfcd0c
Angus Wilson <1103557w@student.gla.ac.uk>
2022-03-22 18:07:13 +0000

    leaderboard test

--- tests/views/tests_leaderboard.py
+++ tests/views/tests_leaderboard.py
@@ -5,23 +5,33 @@ from CR8.models import UserProfile
 
 
 class LeaderboardTests(TestCase):
-    lorne = ""
-
     def setUp(self):
         pass
-        # u1 = User(username="LorneBarnaby")
-        # u1.save()
-        # u1.set_password("Fake Password 19.8")
-        # lorne = UserProfile(user=u1)
-        # lorne.currency = 4000
-        # lorne.save()
 
     def test_response(self):
         response = self.client.get(reverse("cr8:leaderboard"))
-        context = response.context
-        print(context)
         self.assertEqual(response.status_code, 200)
 
-    def test_2(self):
+    def test_no_users(self):
         response = self.client.get(reverse("cr8:leaderboard"))
-        self.assertContains(response,)
+        self.assertContains(response, "Try making some friends")
+
+    def test_users_4(self):
+        for i in range(8, 14):
+            user = User(username=str(i))
+            user.set_password("Fake Password 19.8")
+            user.save()
+            profile = UserProfile(user=user)
+            profile.save()
+        response = self.client.get(reverse("cr8:leaderboard"))
+        self.assertContains(response, "<h5>4</h5>")
+
+    def test_users_cutoff(self):
+        for i in range(10):
+            user = User(username=str(i))
+            user.set_password("Fake Password 19.8")
+            user.save()
+            profile = UserProfile(user=user)
+            profile.save()
+        response = self.client.get(reverse("cr8:leaderboard"))
+        self.assertNotContains(response, "<h5>11</h5>")
