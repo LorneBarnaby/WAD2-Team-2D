032122fa644c1fbb8e81f3607d2d913d02350cd5
Alastair Lyall <2567444l@student.gla.ac.uk>
2022-03-25 13:23:21 +0000

    moved javascript into jquery file

--- cr8_project/settings.py
+++ cr8_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
 
 # Media files (Dynamic)
