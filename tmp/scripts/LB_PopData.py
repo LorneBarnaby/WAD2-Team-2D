365299a4272d362bbbfd1fcc3800aaaf4f7dec80
Lorne Barnaby <lorne123barnaby@gmail.com>
2022-03-25 14:49:10 +0000

    prize list comments

--- tmp/scripts/LB_PopData.py
+++ tmp/scripts/LB_PopData.py
@@ -2,5 +2,10 @@ from CR8.rarity import RARITY
 
 
 def lb_generate_prize_list():
+    """
+    Generate the letters of the alphabet using
+    ASCII codes 65-90
+    :return: list of prizes
+    """
     prize_list = [[f"The Letter {chr(i)}", i, str(RARITY.COMMON), f"Letters/{chr(i)}.png"] for i in range(65, 91)]
     return prize_list
